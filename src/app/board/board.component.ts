82a3a1988fae129ed460681b3209e8d2974c241e
luckyMasharane <lmasharane@gmail.com>
2023-09-12 12:06:46 +0200

    dd

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {MatCardModule} from '@angular/material/card';
+
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
